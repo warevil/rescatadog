aa9b27e82c5c7d5b311985b84e1476608bd1f765
cristianaquino <ycristian10@gmail.com>
2022-06-11 10:02:36 -0500

    dataDog

---
+++ lib/db/dataDog.dart
@@ -0,0 +1,29 @@
+var dogs = [
+  {
+    "image":
+        "https://images.pexels.com/photos/2023384/pexels-photo-2023384.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "name": "Estrella Da",
+    "race": "Astrónomo",
+    "address": "Avenida Miraflores, 1326, Of. 33",
+    "age": 1,
+    "genere": 0,
+  },
+  {
+    "image":
+        "https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg",
+    "name": "Ruben Jano",
+    "race": "Doctor",
+    "address": "Av. Víctor Andrés Belaúnde Mz. C Lt. 10, Int. 4",
+    "age": 2,
+    "genere": 1,
+  },
+  {
+    "image":
+        "https://images.pexels.com/photos/58997/pexels-photo-58997.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "name": "Cristiano Ronaldo",
+    "race": "Futbolista",
+    "address": "Panamericana Sur, Km. 18 (Pista Auxiliar, Altura Grifo Primax)",
+    "age": 3,
+    "genere": 1,
+  },
+];
