2d283607f3db40f69f816f4846d67a009457a4b8
cristianaquino <ycristian10@gmail.com>
2022-07-22 16:47:15 -0500

    structura

--- lib/profile/data/repository.dart
+++ lib/profile/data/repository.dart
@@ -1,12 +1,12 @@
-import 'package:rescatadog/profile/class/pedido.dart';
+import 'package:rescatadog/profile/data/models.dart';
 import 'package:http/http.dart' as http;
 
 class ApiHttpRequest {
   Future<DataProfile?> get dataprofile async {
-    final id = 2;
+    final id = 3;
     final http.Response response = await http.get(
         Uri.parse(
-            'https://prueba-api-pet.herokuapp.com/api/prueba/profile/${id}'),
+            'https://prueba-api-pet.herokuapp.com/api/user/profile/${id}'),
         headers: {"Content-Type": "application/json"});
     if (response.statusCode == 200) {
       print(response.body);
--- lib/profile/page.dart
+++ lib/profile/page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:rescatadog/profile/class/api_profile.dart';
-import 'package:rescatadog/profile/class/pedido.dart';
+import 'package:rescatadog/home/page.dart';
+import 'package:rescatadog/profile/data/repository.dart';
+import 'package:rescatadog/profile/data/models.dart';
 import 'widgets/profile_Item.dart';
-
-import 'package:http/http.dart' as http;
 
 class ProfileUser extends StatefulWidget {
   const ProfileUser({Key? key}) : super(key: key);
@@ -13,24 +12,16 @@ class ProfileUser extends StatefulWidget {
 }
 
 class _ProfileState extends State<ProfileUser> {
-  // Map<String, dynamic> datas = {
-  //   'image': 'https://i.postimg.cc/1XbYvZ2p/woman.jpg',
-  //   'name': 'Mari Doe',
-  //   'email': 'example@email.com',
-  //   'phone': 999999999,
-  //   'address': 'calle s/n 123 av. Sur',
-  //   'aboutme':
-  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-  // };
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
             // leading: IconButton(
             //   icon: Icon(Icons.arrow_back_ios, color: Colors.white),
-            //   onPressed: (){},
+            //   onPressed: () {
+            //     _showSecondPage(context);
+            //   },
             // ),
             title: Text('PERFIL DE USUARIO')),
-        // body: ProfileItem(data));
         body: FutureBuilder<DataProfile?>(
             future: ApiHttpRequest().dataprofile,
             builder: (context, snapshot) {
@@ -42,4 +33,11 @@ class _ProfileState extends State<ProfileUser> {
               return ProfileItem(data);
             }));
   }
+
+  // void _showSecondPage(BuildContext context) {
+  //   final route = MaterialPageRoute(builder: (BuildContext context) {
+  //     return HomePage();
+  //   });
+  //   Navigator.of(context).push(route);
+  // }
 }
